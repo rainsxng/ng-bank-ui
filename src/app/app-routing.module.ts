ab766de24858dbf101171416b7834ecf585f671b
Dmytriy Bohdanov <rainsxng@gmail.com>
2020-05-25 22:15:29 +0300

    feat: main layout structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './containers/layout/layout.component';
+import { DashboardComponent } from './containers/dashboard/dashboard.component';
+import { CardDetailsComponent } from './components/card-details/card-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LayoutComponent, children: [
+      { path: '', redirectTo: '/', pathMatch: 'full' },
+      { path: '', component: DashboardComponent },
+      { path: 'card/:id', component: CardDetailsComponent }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutComponent } from './containers/layout/layout.component';
+import { DashboardComponent } from './containers/dashboard/dashboard.component';
+import { CardDetailsComponent } from './components/card-details/card-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    DashboardComponent,
+    CardDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
